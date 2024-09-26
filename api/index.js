ed639b55291e1819ef0daee1a91fcbc91a739adb
bharathibhuvana24 <bharathibhuvana24@gmail.com>
2024-09-27 01:24:18 +0530

    CONNECTED TO DATABASE

--- api/index.js
+++ api/index.js
@@ -1,5 +1,21 @@
 import express from 'express';
-const app = express();
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+
+
+
+mongoose.connect(process.env.MONGO).then(() =>
+    {
+        console.log("connected to MongoDB!")
+    }).catch((err) => 
+        {console.log("Error:" +err)});
+
+ const app = express();
+
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000!');
     }
