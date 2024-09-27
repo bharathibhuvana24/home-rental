37d1f6e3fba7ad81d20270c4998dda20299ee6df
bharathibhuvana24 <bharathibhuvana24@gmail.com>
2024-09-27 15:44:54 +0530

    created and added with google functionality

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import OAuth from '../components/OAuth';
 
 
 export default function SignIn() {
@@ -80,6 +81,7 @@ export default function SignIn() {
         >
           {loading ? 'Loading...' : 'Sign In'}
         </button>
+        <OAuth />
       </form>
       <div className='flex gap-2 mt-5'>
         <p>Dont have an account?</p>
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import OAuth from '../components/OAuth';
 
 
 export default function SignUp() {
@@ -76,6 +77,7 @@ const handleSubmit = async (e) => {
         >
           {loading ? 'Loading...' : 'Sign Up'}
         </button>
+        <OAuth />
       </form>
 
       <div className="flex gap-2 mt-5">
