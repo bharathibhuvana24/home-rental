2d9e1be807b88f19ae20542d22cd94fb76a17e6e
bharathibhuvana24 <bharathibhuvana24@gmail.com>
2024-09-27 22:11:42 +0530

    created update listing api route

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -104,6 +104,24 @@ export default function profile() {
     }
   };
 
+  const handleListingDelete = async (listingId) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`, {
+        method: 'DELETE',
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        console.log(data.message);
+        return;
+      }
+      setUserListings((prev) =>
+        prev.filter((listing) => listing._id !== listingId)
+      );
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
     
     <div className='p-3 max-w-lg mx-auto'>
@@ -180,7 +198,7 @@ export default function profile() {
         {showListingsError ? 'Error showing listings' : ''}
       </p>
       {userListings &&
-        userListings.length > 0 &&
+        userListings.length > 0 && ( 
         <div className="flex flex-col gap-4">
           <h1 className='text-center mt-7 text-2xl font-semibold'>Your Listings</h1>
           {userListings.map((listing) => (
@@ -203,12 +221,12 @@ export default function profile() {
               </Link>
   
               <div className='flex flex-col item-center'>
-                <button className='text-red-700 uppercase'>Delete</button>
+                <button onClick={() => handleListingDelete(listing._id)} className='text-red-700 uppercase'>Delete</button>
                 <button className='text-green-700 uppercase'>Edit</button>
               </div>
             </div>
           ))}
-        </div>}
+        </div>)}
     </div>
   )
 }
